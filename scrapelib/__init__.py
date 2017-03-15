a440a7e6f250d7ba32a7a579600f5271f8a51969
Skekacs93 <steve@quorum.us>
2017-03-15 18:42:49 -0400

    Minor for logging purposes

--- scrapelib/__init__.py
+++ scrapelib/__init__.py
@@ -326,7 +326,7 @@ class Scraper(CachingSession, ThrottledSession, RetrySession):
                 return resp
             # else sleep 10 seconds
             else:
-                _log.info("Failed request attempt:", resp.content)
+                _log.info("Failed request attempt: %s" % resp.content)
                 if i != (NUM_RETRIES - 1):
                     time.sleep(10)
 
